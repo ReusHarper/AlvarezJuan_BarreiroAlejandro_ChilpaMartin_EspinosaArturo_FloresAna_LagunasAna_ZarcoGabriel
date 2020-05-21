77c3fa038990fa6b438e023e124a995bb63897f8
Reus <artoby99@hotmail.com>
2020-05-21 00:47:40 -0500

    Version 0.0.0.7: Actualización de firebase en el proyecto

--- Files/app/src/main/java/com/example/proyectorelampago/AuthActivity.kt
+++ Files/app/src/main/java/com/example/proyectorelampago/AuthActivity.kt
@@ -2,7 +2,12 @@ package com.example.proyectorelampago
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.ContactsContract
+import androidx.appcompat.app.AlertDialog
 import com.google.firebase.analytics.FirebaseAnalytics
+import com.google.firebase.auth.EmailAuthProvider
+import com.google.firebase.auth.FirebaseAuth
+import kotlinx.android.synthetic.main.activity_auth.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -18,6 +23,43 @@ class MainActivity : AppCompatActivity() {
         bundle.putString("message", "Integracion de Firebase completa")
         analytics.logEvent("InitScreen", bundle)
 
+        // Setup
+        setup()
+    }
+
+    private fun setup{
+        title = "Autenticación"
+
+        signInButton.setOnClickListener {
+            if (emailEditText.text.isNotEmpty() && passwordEditText.text.isNotEmpty()){
+
+                FirebaseAuth.getInstance().createUserWithEmailAndPassword(
+                    emailEditText.text.toString(), passwordEditText.text.toString())
+                    .addOnCompleteListener {
+                        if (it.isSuccessful){
+
+                        } else {
+                            showAlert()
+                        }
+                    }
+                )
+            }
+
+        }
+    }
+
+    private fun showAlert{
+
+        val builder = AlertDialog.Builder(this)
+        builder.setTitle("Error")
+        builder.setMessage("Se ha producido un error de autentificación del usuario")
+        builder.setPositiveButton("Aceptar", null)
+        val dialog: AlertDialog = builder.create()
+        dialog.show()
+
+    }
+
+    private fun showHome(email: String, provider: ProviderType){
 
     }
 }
