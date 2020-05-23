02963c72d8b29ec3e37808f1fb184884c057412a
Reus <artoby99@hotmail.com>
2020-05-22 23:55:13 -0500

    Version 0.0.0.10: Creación y personalización del icono de la app Proyecto-Relampago

--- Files/app/src/main/java/com/example/proyectorelampago/AuthActivity.kt
+++ Files/app/src/main/java/com/example/proyectorelampago/AuthActivity.kt
@@ -1,25 +1,25 @@
 package com.example.proyectorelampago
 
-import androidx.appcompat.app.AppCompatActivity
+import android.R.layout
+import android.content.Intent
 import android.os.Bundle
-import android.provider.ContactsContract
+import android.widget.EditText
 import androidx.appcompat.app.AlertDialog
+import androidx.appcompat.app.AppCompatActivity
 import com.google.firebase.analytics.FirebaseAnalytics
-import com.google.firebase.auth.EmailAuthProvider
 import com.google.firebase.auth.FirebaseAuth
 import kotlinx.android.synthetic.main.activity_auth.*
 
-class MainActivity : AppCompatActivity() {
+
+class AuthActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
-        // Obtain the FirebaseAnalytics instance.
-        val analytics = FirebaseAnalytics.getInstance(this)
-        val bundle = Bundle()
+        setContentView(R.layout.activity_auth)
 
         // Analytics events
+        val analytics = FirebaseAnalytics.getInstance(this)
+        val bundle = Bundle()
         bundle.putString("message", "Integracion de Firebase completa")
         analytics.logEvent("InitScreen", bundle)
 
@@ -27,28 +27,45 @@ class MainActivity : AppCompatActivity() {
         setup()
     }
 
-    private fun setup{
+    private fun setup() {
         title = "Autenticación"
 
         signInButton.setOnClickListener {
             if (emailEditText.text.isNotEmpty() && passwordEditText.text.isNotEmpty()){
 
                 FirebaseAuth.getInstance().createUserWithEmailAndPassword(
-                    emailEditText.text.toString(), passwordEditText.text.toString())
-                    .addOnCompleteListener {
-                        if (it.isSuccessful){
-
+                    "artoby99","246810").addOnCompleteListener {
+                        if(it.isSuccessful){
+                            showHome(it.result?.user?.email ?:"--", ProviderType.BASIC)
+                            //showAlertRegister()
                         } else {
                             showAlert()
                         }
                     }
-                )
+            } else {
+                showAlertNull()
             }
 
         }
+
+        logInButton.setOnClickListener {
+            if (emailEditText.text.isNotEmpty() && passwordEditText.text.isNotEmpty()){
+
+                FirebaseAuth.getInstance().signInWithEmailAndPassword(
+                    emailEditText.text.toString(), passwordEditText.text.toString()).addOnCompleteListener {
+                        if(it.isSuccessful){
+                            showHome(it.result?.user?.email ?:"--", ProviderType.BASIC)
+                        } else {
+                            showAlert()
+                        }
+                    }
+            } else {
+                showAlertNull()
+            }
+        }
     }
 
-    private fun showAlert{
+    private fun showAlert() {
 
         val builder = AlertDialog.Builder(this)
         builder.setTitle("Error")
@@ -59,7 +76,34 @@ class MainActivity : AppCompatActivity() {
 
     }
 
-    private fun showHome(email: String, provider: ProviderType){
+    private fun showAlertNull() {
 
+        val builder = AlertDialog.Builder(this)
+        builder.setTitle("Error")
+        builder.setMessage("Ingrese todos los datos")
+        builder.setPositiveButton("Aceptar", null)
+        val dialog: AlertDialog = builder.create()
+        dialog.show()
+
+    }
+
+    private fun showAlertRegister() {
+
+        val builder = AlertDialog.Builder(this)
+        builder.setTitle("Confirmación")
+        builder.setMessage("Registro completado con éxito.")
+        builder.setPositiveButton("Aceptar", null)
+        val dialog: AlertDialog = builder.create()
+        dialog.show()
+
+    }
+
+    private fun showHome(email: String, provider: ProviderType) {
+        val homeIntent = Intent(this, HomeActivity::class.java).apply {
+            putExtra("email", email)
+            putExtra("provider", provider.name)
+        }
+
+        startActivity(homeIntent)
     }
 }
