22913a0c0d17d34a970b1ba0d1d2d84341fe9a2c
Reus <artoby99@hotmail.com>
2020-05-30 16:51:56 -0500

    Modificación de menú y calendario

--- Files/app/src/main/java/com/example/proyectorelampago/HomeActivity.kt
+++ Files/app/src/main/java/com/example/proyectorelampago/HomeActivity.kt
@@ -1,10 +1,15 @@
 package com.example.proyectorelampago
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.proyectorelampago.R.layout
 import com.google.firebase.auth.FirebaseAuth
+import kotlinx.android.synthetic.main.activity_auth.*
 import kotlinx.android.synthetic.main.activity_home.*
+import kotlinx.android.synthetic.main.activity_home.emailTextView
+import kotlinx.android.synthetic.main.activity_home.logOutbutton
+import kotlinx.android.synthetic.main.activity_menu.*
 
 enum class ProviderType {
     BASIC,
@@ -15,24 +20,39 @@ class HomeActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_home)
+        setContentView(R.layout.activity_menu)
 
         // Setup
         val bundle = intent.extras
         val email = bundle?.getString("email")
-        val provider = bundle?.getString("provider")
-        setup(email ?: "--", provider ?: "--")
+        setup(email ?: "--")
     }
 
-    private fun setup(email: String, provider: String) {
+    private fun setup(email: String) {
 
         title = "Inicio"
         emailTextView.text = email
-        providerTextView.text = provider
+
+        camaraButton.setOnClickListener {
+
+        }
+
+        calendarioButton.setOnClickListener {
+
+        }
+
+        juegosButton.setOnClickListener {
+
+        }
+
+        buzonButton.setOnClickListener {
+
+        }
 
         logOutbutton.setOnClickListener {
             FirebaseAuth.getInstance().signOut()
             onBackPressed()
         }
     }
+    
 }
